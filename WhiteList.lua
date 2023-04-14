7383a3fa19df65115c4a296ebd6f26f4fd837352
MONTIKBOOM <129415993+MONTIKBOOM@users.noreply.github.com>
2023-04-14 10:31:59 +0300

    Update WhiteList.lua

--- WhiteList.lua
+++ WhiteList.lua
@@ -1,7 +1,7 @@
 local whitelistecheck = loadstring(game:HttpGet("https://raw.githubusercontent.com/MONTIKBOOM/WhiteList/main/Data.lua", true))()
 
 if whitelistecheck[game:service('Players').LocalPlayer.UserId] then
-  print("yellow blue vase")
+  print("whitelisted")
 else
   game:service('Players').LocalPlayer:Kick('NOT WHITELISTED')
 end
